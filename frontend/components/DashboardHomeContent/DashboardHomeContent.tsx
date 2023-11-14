3f38a39d83a2899f970310fdf4e67c6d237af1de
Ambisee <celvin.weilington@gmail.com>
2023-11-13 23:10:45 -0800

    Add a button to toggle the entry form into the header part of the page

--- frontend/components/DashboardHomeContent/DashboardHomeContent.tsx
+++ frontend/components/DashboardHomeContent/DashboardHomeContent.tsx
@@ -1,17 +1,25 @@
 "use client"
 
 import { useDashboardData } from "../DashboardDataProvider/DashboardDataProvider"
-import PortalButton from "../PortalButton/PortalButton"
+import ActionButton from "../ActionButton/ActionButton"
+import { useLayout } from "../ProtectedLayoutProvider/ProtectedLayoutProvider"
 
 import styles from "./DashboardHomeContent.module.css"
 
 export default function DashboardHomeContent() {
     const { user, data } = useDashboardData()
+    const { setIsEntryFormToggled } = useLayout()
     
     return (
         <>
-            <div>
+            <div className={styles["page-intro-container"]}>
                 <h2 className={styles["welcome-text"]}>Welcome back, {user?.user_metadata.username}.</h2>
+                <ActionButton 
+                    className={styles["new-entry-button"]}
+                    onClick={() => setIsEntryFormToggled(true)}
+                >
+                    Add a new entry
+                </ActionButton>
             </div>
         </>
     )
