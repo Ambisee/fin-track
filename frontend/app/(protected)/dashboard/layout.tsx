8957849415df10f8197f41f4f8875598e88ae107
Ambisee <celvin.weilington@gmail.com>
2023-11-13 22:55:12 -0800

    Relocate supabase client creation into another file

--- frontend/app/(protected)/dashboard/layout.tsx
+++ frontend/app/(protected)/dashboard/layout.tsx
@@ -1,29 +1,26 @@
 import { ReactNode } from "react"
-import { cookies } from "next/headers"
-import { User, createServerComponentClient } from "@supabase/auth-helpers-nextjs"
+import { User } from "@supabase/auth-helpers-nextjs"
 
 import ProtectedNavbar from "@/components/ProtectedNavbar/ProtectedNavbar"
 import ProtectedLayout from "@/components/ProtectedLayout/ProtectedLayout"
-import { sbKey, sbURL } from "@/supabase/constants"
+import { sbServer } from "@/supabase/supabase_server"
+import ProtectedLayoutProvider from "@/components/ProtectedLayoutProvider/ProtectedLayoutProvider"
 
 interface DashboardLayoutProps {
     children?: ReactNode
 }
 
 export default async function DashboardLayout(props: DashboardLayoutProps) {
-    const sbServer = createServerComponentClient( {cookies: cookies}, {
-        supabaseKey: sbKey,
-        supabaseUrl: sbURL
-    })
-
     const user = (await sbServer.auth.getUser()).data.user as User
     const data = (await sbServer.from("entry").select("*").eq("created_by", user.id))
 
     const layoutContextValue = { user, data }
 
     return (
-        <ProtectedLayout contextValue={layoutContextValue}>
-            {props.children}
-        </ProtectedLayout>
+        <ProtectedLayoutProvider>
+            <ProtectedLayout contextValue={layoutContextValue}>
+                {props.children}
+            </ProtectedLayout>
+        </ProtectedLayoutProvider>
     )
 }
