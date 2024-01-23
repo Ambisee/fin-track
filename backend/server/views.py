a860435d624ba6238d6dbfefdca288117cb0fa4c
Ambisee <celvin.weilington@gmail.com>
2024-01-23 10:45:54 -0800

    Change name of the view function and remove data retrival portion of the view function

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,14 +1,7 @@
-from .data_retrieval import DataRetriever
 from django.http import \
     HttpRequest, \
-    HttpResponse, JsonResponse
+    HttpResponse
 
 
-def foo(req: HttpRequest) -> HttpResponse:
-    retriever = DataRetriever.initialize()
-    res = retriever.get_allow_report_users()
-
-    print(len(res))
-    data = retriever.get_period_data(res[0], 12, 2023)
-
-    return JsonResponse({'data': data})
+def retrieve_all_data(req: HttpRequest) -> HttpResponse:
+    return HttpResponse("Under construction...")
