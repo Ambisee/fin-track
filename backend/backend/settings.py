307e0b9cc0f1f1f1b20d1b56e5fa997be4d48ae7
Ambisee <celvin.weilington@gmail.com>
2024-06-21 16:15:12 -0700

    Add a Django settings variable for static root

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -134,6 +134,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if os.getenv("STATIC_ROOT") is not None:
+    STATIC_ROOT = os.getenv("STATIC_ROOT")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
