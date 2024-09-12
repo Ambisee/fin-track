2e61c1375443bb62458eaf954959b47c8b074723
Ambisee <celvin.weilington@gmail.com>
2024-09-12 21:34:18 +0700

    Fix error where hideCloseButton props is passed as an HTML element's attribute

--- frontend/src/components/ui/dialog.tsx
+++ frontend/src/components/ui/dialog.tsx
@@ -34,7 +34,7 @@ const DialogContent = React.forwardRef<
 	React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
 		hideCloseButton?: boolean
 	}
->(({ className, children, ...props }, ref) => (
+>(({ className, children, hideCloseButton, ...props }, ref) => (
 	<DialogPortal>
 		<DialogOverlay />
 		<DialogPrimitive.Content
@@ -46,7 +46,7 @@ const DialogContent = React.forwardRef<
 			{...props}
 		>
 			{children}
-			{!props.hideCloseButton && (
+			{!hideCloseButton && (
 				<DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
 					<X className="h-4 w-4" />
 					<span className="sr-only">Close</span>
@@ -113,15 +113,14 @@ const DialogDescription = React.forwardRef<
 DialogDescription.displayName = DialogPrimitive.Description.displayName
 
 export {
-    Dialog,
-    DialogClose,
-    DialogContent,
-    DialogDescription,
-    DialogFooter,
-    DialogHeader,
-    DialogOverlay,
-    DialogPortal,
-    DialogTitle,
-    DialogTrigger
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogOverlay,
+	DialogPortal,
+	DialogTitle,
+	DialogTrigger
 }
-
