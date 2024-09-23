708ee69f15e56af6c20427097827e526b03a8713
Ambisee <celvin.weilington@gmail.com>
2024-09-23 20:13:09 +0700

    Add search feature to the entry page

--- frontend/src/app/(protected)/dashboard/entries/page.client.tsx
+++ frontend/src/app/(protected)/dashboard/entries/page.client.tsx
@@ -1,43 +1,87 @@
 "use client"
 
+import { Button } from "@/components/ui/button"
+import { Dialog, DialogContent } from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input"
 import { Skeleton } from "@/components/ui/skeleton"
 import EntryList from "@/components/user/EntryList"
 import { useEntryDataQuery, useUserQuery } from "@/lib/hooks"
 import { sortDataByDateGroup } from "@/lib/utils"
-import { useMemo } from "react"
+import { ChevronLeft, ChevronRight } from "lucide-react"
+import { useContext, useEffect, useMemo, useRef, useState } from "react"
+import { DashboardContext } from "../layout"
+import { SearchResult } from "minisearch"
 
 export default function DashboardEntries() {
+	const [curIndex, setCurIndex] = useState(-1)
+	const [searchQuery, setSearchQuery] = useState<string>("")
+	const [searchResult, setSearchResult] = useState<SearchResult[] | null>(null)
+	const { search } = useContext(DashboardContext)
+
 	const userQuery = useUserQuery()
-	const entriesQuery = useEntryDataQuery()
+	const entryQuery = useEntryDataQuery()
 
 	const dataGroups = useMemo(() => {
-		if (entriesQuery.isLoading) {
+		if (entryQuery.isLoading) {
 			return []
 		}
 
-		if (entriesQuery.data === undefined || entriesQuery.data.data === null) {
+		if (entryQuery.data === undefined || entryQuery.data.data === null) {
 			return []
 		}
 
-		return sortDataByDateGroup(entriesQuery.data.data)
-	}, [entriesQuery.data, entriesQuery.isLoading])
+		return sortDataByDateGroup(entryQuery.data.data)
+	}, [entryQuery.data, entryQuery.isLoading])
+
+	const renderSearchResult = () => {
+		if (
+			entryQuery.isLoading ||
+			!entryQuery.data?.data ||
+			searchResult === null
+		) {
+			return []
+		}
+
+		const data = []
+		for (const value of searchResult) {
+			data.push(entryQuery.data.data[value.id])
+		}
+
+		return (
+			<div className="mt-14">
+				<EntryList data={data} />
+			</div>
+		)
+	}
 
 	const renderEntries = () => {
-		if (entriesQuery.isLoading) {
+		let index = curIndex
+		let isIndexInitialized = true
+		if (index == -1) {
+			if (dataGroups.length > 0) {
+				const today = new Date()
+				const firstPeriod = new Date(
+					`${dataGroups[0].month}-${dataGroups[0].year}`
+				)
+				const index =
+					12 * (today.getFullYear() - firstPeriod.getFullYear()) +
+					(today.getMonth() - firstPeriod.getMonth())
+
+				setCurIndex(index)
+			}
+
+			isIndexInitialized = false
+		}
+
+		if (entryQuery.isLoading || !isIndexInitialized) {
 			return (
 				<>
 					<div className="mb-8">
-						<Skeleton className="w-32 h-8 mb-4" />
-						<Skeleton className="w-full h-[6.25rem] mb-4" />
-						<Skeleton className="w-full h-[6.25rem] mb-4" />
-					</div>
-					<div>
-						<Skeleton className="w-32 h-8 mb-4" />
-						<Skeleton className="w-full h-[6.25rem] mb-4" />
-						<Skeleton className="w-full h-[6.25rem] mb-4" />
-					</div>
-					<div className="mb-8">
-						<Skeleton className="w-32 h-8 mb-4" />
+						<div className="w-full flex justify-between items-center mb-4 pb-4">
+							<Skeleton className="w-8 h-8" />
+							<Skeleton className="w-36 h-8" />
+							<Skeleton className="w-8 h-8" />
+						</div>
 						<Skeleton className="w-full h-[6.25rem] mb-4" />
 						<Skeleton className="w-full h-[6.25rem] mb-4" />
 					</div>
@@ -45,22 +89,57 @@ export default function DashboardEntries() {
 			)
 		}
 
-		return dataGroups.map((value) => {
-			return (
-				<div key={value.month + value.year} className="my-8">
-					<h3 className="text-lg mb-4">
-						{value.month} {value.year}
-					</h3>
-					<EntryList data={value.data} />
+		return (
+			<div className="mb-8">
+				<div className="flex justify-between items-center pb-4 bg-background">
+					<Button
+						className="w-fit h-fit p-0 hover:bg-background"
+						variant="ghost"
+						disabled={curIndex === dataGroups.length - 1}
+						onClick={() =>
+							setCurIndex((c) => Math.min(dataGroups.length - 1, c + 1))
+						}
+					>
+						<ChevronLeft className="w-4 h-4" />
+					</Button>
+					<Button variant="ghost" className="text-lg" asChild>
+						<h3 className="text-lg hover:cursor-pointer">
+							{dataGroups[curIndex].month} {dataGroups[curIndex].year}
+						</h3>
+					</Button>
+					<Button
+						className="w-fit h-fit p-0 hover:bg-background"
+						variant="ghost"
+						disabled={curIndex === 0}
+						onClick={() => setCurIndex((c) => Math.max(0, c - 1))}
+					>
+						<ChevronRight className="w-4 h-4" />
+					</Button>
 				</div>
-			)
-		})
+				<EntryList data={dataGroups[curIndex].data} />
+			</div>
+		)
 	}
 
 	return (
 		<>
-			<h1 className="text-2xl mb-8">Entries</h1>
-			{renderEntries()}
+			<h1 className="text-2xl">Entries</h1>
+			<div className="sticky top-0 py-4 bg-background">
+				<Input
+					placeholder="Search for an entry..."
+					value={searchQuery}
+					onChange={(e) => {
+						setSearchQuery(e.target.value)
+						if (e.target.value === "") {
+							setSearchResult(null)
+							return
+						}
+
+						setSearchResult(search.search(e.target.value, { prefix: true }))
+					}}
+				/>
+			</div>
+			{searchQuery !== "" ? renderSearchResult() : renderEntries()}
 		</>
 	)
 }
--- frontend/src/app/(protected)/dashboard/layout.tsx
+++ frontend/src/app/(protected)/dashboard/layout.tsx
@@ -1,13 +1,23 @@
 "use client"
 
+import MiniSearch from "minisearch"
 import { Dialog } from "@/components/ui/dialog"
+
 import EntryForm from "@/components/user/EntryForm/EntryForm"
 import ProtectedNavbar from "@/components/user/ProtectedNavbar"
 import useGlobalStore from "@/lib/store"
+import { useEntryDataQuery, useUserQuery } from "@/lib/hooks"
+import { createContext, useEffect, useRef } from "react"
 
 interface DashboardLayoutProps {
 	children: JSX.Element
 }
+
+interface DashboardContextObject {
+	search: MiniSearch
+}
+
+const DashboardContext = createContext<DashboardContextObject>(null!)
 
 function LayoutDialog(props: { children: JSX.Element }) {
 	const open = useGlobalStore((state) => state.open)
@@ -27,16 +37,53 @@ function LayoutEntryForm() {
 	return <EntryForm data={data} onSubmitSuccess={onSubmitSuccess} />
 }
 
-export default function DashboardLayout(props: DashboardLayoutProps) {
+function DashboardContextProvider(props: { children: JSX.Element }) {
+	const searchRef = useRef(
+		new MiniSearch({
+			fields: ["index", "amount", "date", "category.name", "note"],
+			storeFields: ["index"],
+			idField: "index",
+			extractField: (document, fieldName) => {
+				return fieldName
+					.split(".")
+					.reduce((doc, key) => doc && doc[key], document)
+			}
+		})
+	)
+	const entryQuery = useEntryDataQuery()
+
+	useEffect(() => {
+		if (!entryQuery.data?.data) {
+			return
+		}
+
+		searchRef.current.removeAll()
+		searchRef.current.addAll(
+			entryQuery.data.data.map((val, index) => ({ ...val, index: index }))
+		)
+	}, [entryQuery])
+
 	return (
-		<LayoutDialog>
-			<>
-				<div className="dashboard-content" vaul-drawer-wrapper="">
-					{props.children}
-				</div>
-				<ProtectedNavbar />
-				<LayoutEntryForm />
-			</>
-		</LayoutDialog>
+		<DashboardContext.Provider value={{ search: searchRef.current }}>
+			{props.children}
+		</DashboardContext.Provider>
 	)
 }
+
+export default function DashboardLayout(props: DashboardLayoutProps) {
+	return (
+		<DashboardContextProvider>
+			<LayoutDialog>
+				<>
+					<div className="dashboard-content" vaul-drawer-wrapper="">
+						{props.children}
+					</div>
+					<ProtectedNavbar />
+					<LayoutEntryForm />
+				</>
+			</LayoutDialog>
+		</DashboardContextProvider>
+	)
+}
+
+export { DashboardContext }
