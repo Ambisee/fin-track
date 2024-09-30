bae110604fcb0e510db6a7d77ad08257c1b9e8ee
Ambisee <celvin.weilington@gmail.com>
2024-09-30 17:50:31 +0700

    Complete the month picker on the entries page

--- frontend/src/app/(protected)/dashboard/entries/page.client.tsx
+++ frontend/src/app/(protected)/dashboard/entries/page.client.tsx
@@ -6,11 +6,149 @@ import { Skeleton } from "@/components/ui/skeleton"
 import EntryList from "@/components/user/EntryList"
 import { useEntryDataQuery } from "@/lib/hooks"
 import { getDataGroup, sortDataByDateGroup } from "@/lib/utils"
-import { ChevronLeft, ChevronRight } from "lucide-react"
+import { ChevronLeft, ChevronRight, X } from "lucide-react"
 import { useContext, useEffect, useMemo, useState, useTransition } from "react"
 import { DashboardContext } from "../layout"
 import { SearchResult } from "minisearch"
 import { MONTHS } from "@/lib/constants"
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger
+} from "@/components/ui/dialog"
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue
+} from "@/components/ui/select"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormField } from "@/components/ui/form"
+import { useToast } from "@/components/ui/use-toast"
+import { ReloadIcon } from "@radix-ui/react-icons"
+
+interface MonthPickerProps {
+	value: number[]
+	onValueChange: (value: number[]) => void
+}
+
+const monthPickerFormSchema = z.object({
+	month: z.string(),
+	year: z.coerce.number().min(0)
+})
+
+function MonthPicker(props: MonthPickerProps) {
+	const { toast } = useToast()
+	const [open, setOpen] = useState(false)
+
+	const form = useForm<z.infer<typeof monthPickerFormSchema>>({
+		resolver: zodResolver(monthPickerFormSchema),
+		defaultValues: {
+			month: MONTHS[props.value[0]],
+			year: props.value[1]
+		}
+	})
+
+	return (
+		<Dialog open={open} onOpenChange={setOpen}>
+			<DialogContent onCloseAutoFocus={(e) => e.preventDefault()}>
+				<Form {...form}>
+					<form
+						onSubmit={(e) => {
+							e.preventDefault()
+							form.handleSubmit(
+								(formData) => {
+									console.log("suc")
+									props.onValueChange([
+										MONTHS.indexOf(formData.month),
+										formData.year
+									])
+									setOpen(false)
+								},
+								(error) => {
+									console.log("err")
+									toast({
+										variant: "destructive",
+										description: (
+											<div>
+												{error.month?.message && <p>{error.month.message}</p>}
+												{error.year?.message && <p>{error.year.message}</p>}
+											</div>
+										)
+									})
+								}
+							)()
+						}}
+					>
+						<DialogHeader>
+							<DialogTitle>Select a month</DialogTitle>
+							<DialogDescription>
+								Please choose the month to view the month&apos;s data
+							</DialogDescription>
+						</DialogHeader>
+						<div className="w-full my-4 flex items-center justify-between gap-4">
+							<FormField
+								control={form.control}
+								name="month"
+								render={({ field }) => (
+									<FormControl>
+										<Select
+											defaultValue={field.value}
+											onValueChange={field.onChange}
+										>
+											<SelectTrigger>
+												<SelectValue placeholder="Select a month..." />
+											</SelectTrigger>
+											<SelectContent className="max-h-60">
+												<SelectGroup>
+													{MONTHS.map((value, index) => {
+														return (
+															<SelectItem key={value} value={value}>
+																{value}
+															</SelectItem>
+														)
+													})}
+												</SelectGroup>
+											</SelectContent>
+										</Select>
+									</FormControl>
+								)}
+							/>
+							<FormField
+								control={form.control}
+								name="year"
+								render={({ field }) => (
+									<FormControl>
+										<Input inputMode="numeric" {...field} />
+									</FormControl>
+								)}
+							/>
+						</div>
+						<DialogFooter>
+							<Button className="w-full">Update</Button>
+						</DialogFooter>
+					</form>
+				</Form>
+			</DialogContent>
+			<DialogTrigger asChild>
+				<Button variant="ghost" className="text-lg" onClick={() => {}} asChild>
+					<h3 className="text-lg hover:cursor-pointer">
+						{MONTHS[props.value[0]]} {props.value[1]}
+					</h3>
+				</Button>
+			</DialogTrigger>
+		</Dialog>
+	)
+}
 
 export default function DashboardEntries() {
 	const [curPeriod, setCurPeriod] = useState<number[] | undefined>(undefined)
@@ -75,9 +213,9 @@ export default function DashboardEntries() {
 			return (
 				<div className="mb-8">
 					<div className="w-full flex justify-between items-center mb-4 pb-4">
-						<Skeleton className="w-14 h-14" />
-						<Skeleton className="w-36 h-14" />
-						<Skeleton className="w-14 h-14" />
+						<Skeleton className="w-12 h-12 rounded-full" />
+						<Skeleton className="w-36 h-12" />
+						<Skeleton className="w-12 h-12 rounded-full" />
 					</div>
 					<Skeleton className="w-full h-[6.25rem] mb-4" />
 					<Skeleton className="w-full h-[6.25rem] mb-4" />
@@ -109,11 +247,13 @@ export default function DashboardEntries() {
 					>
 						<ChevronLeft className="w-4 h-4" />
 					</Button>
-					<Button variant="ghost" className="text-lg" asChild>
-						<h3 className="text-lg hover:cursor-pointer">
-							{currentGroup.month} {currentGroup.year}
-						</h3>
-					</Button>
+					<MonthPicker
+						key={`${curPeriod[0]}-${curPeriod[1]}`}
+						value={curPeriod}
+						onValueChange={(value) => {
+							setCurPeriod(value)
+						}}
+					/>
 					<Button
 						className="h-12 w-12 rounded-full"
 						variant="ghost"
@@ -149,7 +289,7 @@ export default function DashboardEntries() {
 	}, [curPeriod])
 
 	return (
-		<>
+		<div>
 			<h1 className="text-2xl">Entries</h1>
 			<div className="sticky top-0 py-4 bg-background">
 				<Input
@@ -169,6 +309,6 @@ export default function DashboardEntries() {
 				/>
 			</div>
 			{searchQuery !== "" ? renderSearchResult() : renderEntries()}
-		</>
+		</div>
 	)
 }
