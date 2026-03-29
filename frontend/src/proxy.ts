2db3b3e35d050b23243d0f285cde90e50708fb27
Ambisee <celvin.weilington@gmail.com>
2026-03-29 16:12:45 +0700

    feat: Update application proxy.
    - Replace calls to `getUser` with `getClaims` for credential checks.
    - Replace `console.log` calls with `console.error`.
    - Reformat `proxy.ts` file.

--- frontend/src/proxy.ts
+++ frontend/src/proxy.ts
@@ -3,52 +3,58 @@ import { NextRequest, NextResponse } from "next/server"
 import { supabaseMiddlewareClient } from "./lib/supabase"
 
 function isProtectedUrl(url: NextURL) {
-    return url.pathname.startsWith("/dashboard")
+	return url.pathname.startsWith("/dashboard")
 }
 
 export async function proxy(request: NextRequest) {
 	const response = NextResponse.next({ request })
 	const supabase = supabaseMiddlewareClient(request, response)
 
-    /**
-     * Redirect unauthorized user to the signin page when accessing the password recovery page
-     */
-    if (request.nextUrl.pathname.startsWith("/recovery")) {
-        const token_hash = request.nextUrl.searchParams.get("token_hash")
+	/**
+	 * Redirect unauthorized user to the signin page when accessing the password recovery page
+	 */
+	if (request.nextUrl.pathname.startsWith("/recovery")) {
+		const token_hash = request.nextUrl.searchParams.get("token_hash")
 
-        if (token_hash === null) {
-            console.log("No token_hash")
-            return NextResponse.redirect(new URL("/", request.url))
-        }
+		if (token_hash === null) {
+			console.error("No token_hash")
+			return NextResponse.redirect(new URL("/", request.url))
+		}
 
-        const otpVerification = await supabase.auth.verifyOtp({ type: "email", token_hash: token_hash })
-        if (otpVerification.error !== null) {
-            console.log("OTP verification failed")
-            console.log("Reason:", otpVerification.error.message)
-            return NextResponse.redirect(new URL("/sign-in", request.url))
-        }
-        
-        return response
-    }
+		const otpVerification = await supabase.auth.verifyOtp({
+			type: "email",
+			token_hash: token_hash
+		})
+		if (otpVerification.error !== null) {
+			console.error("OTP verification failed.")
+			console.error(`Reason: ${otpVerification.error.message}.`)
+			return NextResponse.redirect(new URL("/sign-in", request.url))
+		}
 
-    const { data: { user } } = await supabase.auth.getUser().catch(() => ({data: {user: null}}))
+		return response
+	}
 
-    /**
-     * Redirect unauthenticated user to the login page when accessing the dashboard
-     */
-    if (user === null && isProtectedUrl(request.nextUrl)) {
-        return NextResponse.redirect(new URL("/sign-in", request.url))
-    }
+	const { data, error } = await supabase.auth.getClaims()
 
-    return response
+	/**
+	 * Redirect unauthenticated user to the login page when accessing the dashboard
+	 */
+	if (data === null && isProtectedUrl(request.nextUrl)) {
+		console.error(
+			`Unauthorized access to protected URL. Reason: ${error?.message}.`
+		)
+		return NextResponse.redirect(new URL("/sign-in", request.url))
+	}
+
+	return response
 }
 
 export const config = {
-    matcher: [
-        "/",
-        "/recovery",
-        "/dashboard/:path*",
-        "/sign-in/:path*",
-        "/sign-up/:path*",
-    ]
+	matcher: [
+		"/",
+		"/recovery",
+		"/dashboard/:path*",
+		"/sign-in/:path*",
+		"/sign-up/:path*"
+	]
 }
