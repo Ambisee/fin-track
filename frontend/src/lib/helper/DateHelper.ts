bfd35255fd932bc2eee236db2b0191f8c49221ab
Ambisee <celvin.weilington@gmail.com>
2026-04-19 16:38:35 +0700

    feat: Update `DateHelper` class.
    - Add a function for getting the range of timestamp representing the start and end of the given date's week.
    - Update to use `it.each` for running testcases.

--- frontend/src/lib/helper/DateHelper.ts
+++ frontend/src/lib/helper/DateHelper.ts
@@ -1,5 +1,10 @@
 type DateRange = { from: Date; to: Date }
 
+interface UTS35WeekElement {
+	firstDay: number
+	weekends: [number]
+}
+
 class DateHelper {
 	static toDatabaseString(date: Date): string {
 		const year = String(date.getFullYear())
@@ -9,13 +14,68 @@ class DateHelper {
 		return `${year}-${month}-${day}`
 	}
 
+	/**
+	 * Return the the timestamp ranges of the given date's month.
+	 *
+	 * @param date
+	 * @returns
+	 */
 	static getMonthStartEnd(date: Date): DateRange {
 		const from = new Date(date)
 		from.setDate(1)
+		from.setHours(0, 0, 0, 0)
 
 		const to = new Date(date)
+		to.setHours(0, 0, 0, 0)
 		to.setMonth(to.getMonth() + 1)
-		to.setDate(0)
+		to.setDate(1)
+		to.setTime(to.getTime() - 1)
+
+		return { from, to }
+	}
+
+	static getWeekStartEnd(
+		date: Date,
+		locale: Intl.Locale = new Intl.Locale(navigator.language)
+	): DateRange {
+		// Defaults to Monday as the first day of the week.
+		let firstDayOfTheWeek = 1
+
+		// Checks for available web APIs for retrieving UTS25WeekElement info.
+		if ("getWeekInfo" in locale && typeof locale.getWeekInfo === "function") {
+			const weekInfo: UTS35WeekElement = locale.getWeekInfo()
+			firstDayOfTheWeek = weekInfo.firstDay
+		} else if (
+			"weekInfo" in locale &&
+			typeof locale.weekInfo === "object" &&
+			locale.weekInfo !== null &&
+			"firstDay" in locale.weekInfo &&
+			typeof locale.weekInfo.firstDay === "number"
+		) {
+			firstDayOfTheWeek = locale.weekInfo.firstDay
+		}
+
+		firstDayOfTheWeek -= 1 // Project to the range of [0,6] instead.
+
+		// Get current day in the range of [0, 6] (Sunday to Saturday).
+		let day = date.getDay()
+
+		// Convert current day to range [0, 6] (Monday to Saturday).
+		if (day < 1) {
+			day = 7
+		}
+		day -= 1
+
+		const offset = (firstDayOfTheWeek - (day + 7)) % 7
+
+		const from = new Date(date)
+		from.setDate(date.getDate() + offset)
+		from.setHours(0, 0, 0, 0)
+
+		const to = new Date(from)
+		to.setDate(from.getDate() + 7)
+		to.setHours(0, 0, 0, 0)
+		to.setTime(to.getTime() - 1)
 
 		return { from, to }
 	}
--- frontend/tests/lib/helper/DateHelper.test.ts
+++ frontend/tests/lib/helper/DateHelper.test.ts
@@ -1,23 +1,127 @@
-import { DateHelper } from "@/lib/helper/DateHelper"
+import { DateHelper, DateRange } from "@/lib/helper/DateHelper"
 
 interface TestCase<I, E> {
 	input: I
 	expected: E
 }
 
-describe("DateHelper tests", () => {
-	const testcases: TestCase<Date, string>[] = [
-		{ input: new Date("2024-12-12"), expected: "2024-12-12" },
-		{ input: new Date("2024/01/01"), expected: "2024-01-01" },
-		{ input: new Date("2024.02.01"), expected: "2024-02-01" },
-		{ input: new Date("May 31, 2024"), expected: "2024-05-31" },
-		{ input: new Date("02, 28, 2025"), expected: "2025-02-28" }
-	]
+interface GetWeekStartEndInput {
+	date: Date
+	locale: string
+}
 
-	it("should format date strings into Postgres' date format", () => {
-        for (let i = 0; i < testcases.length; i++) {
-            const actual = DateHelper.toDatabaseString(testcases[i].input)
-            expect(actual).toEqual(testcases[i].expected)
-        }
-    })
+describe("DateHelper tests", () => {
+	describe("toDateString tests", () => {
+		const testcases: TestCase<Date, string>[] = [
+			{ input: new Date("2024-12-12"), expected: "2024-12-12" },
+			{ input: new Date("2024/01/01"), expected: "2024-01-01" },
+			{ input: new Date("2024.02.01"), expected: "2024-02-01" },
+			{ input: new Date("May 31, 2024"), expected: "2024-05-31" },
+			{ input: new Date("02, 28, 2025"), expected: "2025-02-28" }
+		]
+
+		it.each(testcases)(
+			"should return the correct Postgres date representation for input: [date] $input",
+			({ input, expected }) => {
+				const actual = DateHelper.toDatabaseString(input)
+				expect(actual).toEqual(expected)
+			}
+		)
+	})
+
+	describe("getMonthStartEnd tests", () => {
+		const testcases: TestCase<Date, DateRange>[] = [
+			{
+				input: new Date("2025-12-12"),
+				expected: {
+					from: new Date("2025-12-01, 00:00:00.000"),
+					to: new Date("2025-12-31, 23:59:59.999")
+				}
+			},
+			{
+				input: new Date("2026-01-14"),
+				expected: {
+					from: new Date("2026-01-01, 00:00:00.000"),
+					to: new Date("2026-01-31, 23:59:59.999")
+				}
+			}
+		]
+
+		it.each(testcases)(
+			"should return the correct timestamp for input: [date] $input",
+			({ input, expected }) => {
+				const actual = DateHelper.getMonthStartEnd(input)
+				expect(actual).toEqual(expected)
+			}
+		)
+	})
+
+	describe("getWeekStartEnd tests", () => {
+		const idLocale = "id"
+		const usLocale = "en-us"
+		const euLocale = "en-eu"
+		const egyptLocale = "ar-eg"
+
+		const testcases: TestCase<GetWeekStartEndInput, DateRange>[] = [
+			{
+				input: {
+					date: new Date("2025-12-12"),
+					locale: idLocale
+				},
+				expected: {
+					from: new Date("2025-12-07, 00:00:00.000"),
+					to: new Date("2025-12-13, 23:59:59.999")
+				}
+			},
+			{
+				input: {
+					date: new Date("2024/02/25"),
+					locale: idLocale
+				},
+				expected: {
+					from: new Date("2024-02-25, 00:00:00.000"),
+					to: new Date("2024-03-02, 23:59:59.999")
+				}
+			},
+			{
+				input: {
+					date: new Date("2024.03.31"),
+					locale: usLocale
+				},
+				expected: {
+					from: new Date("2024-03-31, 00:00:00.000"),
+					to: new Date("2024-04-06, 23:59:59.999")
+				}
+			},
+			{
+				input: {
+					date: new Date("2023-10-06"),
+					locale: egyptLocale
+				},
+				expected: {
+					from: new Date("2023-09-30, 00:00:00.000"),
+					to: new Date("2023-10-06, 23:59:59.999")
+				}
+			},
+			{
+				input: {
+					date: new Date("2023-06-29"),
+					locale: euLocale
+				},
+				expected: {
+					from: new Date("2023-06-26, 00:00:00.000"),
+					to: new Date("2023-07-02, 23:59:59.999")
+				}
+			}
+		]
+
+		it.each(testcases)(
+			"should return the correct timestamp range: [date] $input.date, [locale] $input.locale",
+			({ input, expected }) => {
+				const locale = new Intl.Locale(input.locale)
+				const actual = DateHelper.getWeekStartEnd(input.date, locale)
+				expect(actual).toEqual(expected)
+			}
+		)
+	})
 })
