712d2e488eba80eb970f29c3481b90843145ec9e
Ambisee <celvin.weilington@gmail.com>
2026-05-09 11:14:22 +0700

    feat: Update shadcn components.
    - Add the `InputGroup` component.
    - Update the `Input` and `TextArea` components.
    - Adjust the Tailwind class names on the `ComboBox` component.

--- frontend/src/components/ui/combobox.tsx
+++ frontend/src/components/ui/combobox.tsx
@@ -34,7 +34,7 @@ export default function ComboBox(props: ComboBoxProps) {
 					variant="outline"
 					role="combobox"
 					className={cn(
-						"w-[200px] justify-between",
+						"w-50 justify-between",
 						!props.value && "text-muted-foreground"
 					)}
 				>
@@ -42,7 +42,7 @@ export default function ComboBox(props: ComboBoxProps) {
 					<CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
 				</Button>
 			</PopoverTrigger>
-			<PopoverContent className="w-[200px] p-0">
+			<PopoverContent className="w-50 p-0">
 				<Command>
 					<CommandInput placeholder="Select type..." className="h-9" />
 					<CommandList>
